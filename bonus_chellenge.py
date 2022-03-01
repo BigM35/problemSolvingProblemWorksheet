220a6ea7fe7a378192705c207accb73f218e635b
Manny M <bigm@Mannys-MacBook-Pro.local>
2022-03-01 09:18:48 -0600

    change function so that it includes even palindrome

--- bonus_chellenge.py
+++ bonus_chellenge.py
@@ -13,20 +13,28 @@
 # if it gets to the middle letter its Palindrome.
 
 def check_palindrome():
-    user_input = "level"
+    user_input = input("Enter a word to to check if its a palindrome or not: ")
     front_val = ""
     back_val = ""
     if len(user_input) % 2 == 1:
         mid_index = int(len(user_input) / 2)
         for character in range(0,mid_index):
             front_val += user_input[character]
-        for char in range(len(user_input) - 1, not mid_index, -1):
-            back_val = user_input[char]
-            if front_val in back_val:
-                print(f"{user_input} is a Palindrome!")
-                    
-    
+        for char in user_input[:- mid_index - 1: - 1]:
+            back_val += char
+        if front_val == back_val:
+            print(f"{user_input} is a Palindrome!")
+        else:
+            print (f"{user_input} is not a Palindrome!")            
     else:
-        print (f"{user_input} is not a Palindrome!")
-
+        mid_index = int(len(user_input) / 2) + 1
+        for character in range(0,mid_index):
+            front_val += user_input[character]
+        for char in user_input[:- mid_index - 1: - 1]:
+            back_val += char
+        if front_val == back_val:
+            print(f"{user_input} is a Palindrome!")
+        else:
+            print (f"{user_input} is not a Palindrome!")
+       
 check_palindrome()
